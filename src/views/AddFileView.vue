422708c41be07751845c17ff5b4b2c17091c253f
rzy <815459626@qq.com>
2023-10-11 21:59:13 +0800

    实现可被后端以file形式接收 为实现以form形式接收

--- src/views/AddFileView.vue
+++ src/views/AddFileView.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="mb-3">
         <label for="formFile" class="form-label">上传文件</label>
-        <input @change="upload" class="form-control" type="file" id="formFile">
+        <input @change="upload" class="form-control" type="file" id="formFile" name="filename">
     </div>
 </template>
 
 <script>
 // import { ref } from 'vue';
-// import $ from 'jquery';
+import $ from 'jquery';
 import { useStore } from 'vuex';
+import axios from 'axios';
 // import { ref } from 'vue';
 
 export default {
@@ -18,32 +19,48 @@ export default {
         const store = useStore();
         const upload = (e) => {
             // var that = this;
-            var fileObj = e.target.files[0];
-            console.log(fileObj);
+            const files = Array.from(e.target.files);
+            var file = files[0];
+            console.log(files[0]);
 
-            var formdata = new FormData();
-            formdata.append('test', fileObj);
-            console.log(formdata);
+            const formData = new FormData();
+            formData.append('file', file);
+            console.log(formData.get('file'));
 
-            // $.ajax({
-            //     url: 'http://192.168.43.171:8000/uploadfile',
-            //     type: 'POST',
-            //     data: {
-            //         title: 'test',
-            //         article: formdata,
-            //         user_id: store.state.user.id,
-            //     },
+            $.ajax({
+                url: 'http://192.168.43.171:8000/uploadfile',
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    transformRequest: [file => file],
+                },
 
-            //     success: (resp) => {
-            //         if (resp.result === 'success') {
-            //             console.log('上传成功');
-            //         } else {
-            //             console.log('api调用成功 上传文件失败');
-            //         }
-            //     },
-            //     error: () => {
-            //         console.log('api调用失败');
+                success: (resp) => {
+                    if (resp.result === 'success') {
+                        console.log('上传成功');
+                    } else {
+                        console.log('api调用成功 上传文件失败');
+                    }
+                },
+                error: () => {
+                    console.log('api调用失败');
+                }
+            });
+
+            // axios({
+            //     method: "post",
+            //     url: "http://192.168.43.171:8000/uploadfile",
+            //     data: formData,
+            //     headers: {
+            //         "Content-Type": "multipart/form-data",
+            //         transformRequest: [file => file],
             //     }
+            // }).then((resp) => {
+            //     console.log('resp.result');
+            //     console.log(resp.result);
             // })
         };
 
